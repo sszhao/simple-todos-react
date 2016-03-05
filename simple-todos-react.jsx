d99e9489272b4889ddaa67b80f56f8009683bfaf
Shengdong Zhao <zhaosd@comp.nus.edu.sg>
2016-03-05 23:54:29 +0800

    Add model data from meteor

--- simple-todos-react.jsx
+++ simple-todos-react.jsx
@@ -1,8 +1,24 @@
+// Define a collection to hold our tasks
+Tasks = new Mongo.Collection("tasks");
+
 if (Meteor.isClient) {
   // This code is executed on the client only
  
   Meteor.startup(function () {
     // Use Meteor.startup to render the component after the page is ready
     ReactDOM.render(<App />, document.getElementById("render-target"));
+  });
+}
+
+if (Meteor.isServer) {
+  // This code is executed on the client only
+ 
+  Meteor.startup(function () {
+    // Use Meteor.startup to render the component after the page is ready
+    if(Tasks.find({}).count() === 0){
+    	Tasks.insert({ text: "Introduce Meteor", createAt: new Date()});
+    	Tasks.insert({ text: "Introduce React", createAt: new Date()});
+    	Tasks.insert({ text: "Introduce Meteor + React", createAt: new Date()});
+    }
   });
 }
