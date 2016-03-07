ead00683bf068736d808a4e279869d157ab9ca9e
Shengdong Zhao <zhaosd@comp.nus.edu.sg>
2016-03-07 20:03:27 +0800

    Adjusted the explanation for steps. ItemCompleted is no longer a state

--- simple-todos-react.jsx
+++ simple-todos-react.jsx
@@ -37,7 +37,7 @@
 		TaskList
 			tasks : {Collection} [State? Yes]
 		Task 
-			completed : {true, false} [State? Yes]
+			completed : {true, false} [State? No, this can be derived from Tasks] Note that I made a mistake to think of it as state in the first draft. 
 			visible : {true, false} [State? No, can be derived from	Tasks & SelectionScope]
 			todoText : String [State? No, can be derived from Tasks] 
 		ControlBar 
@@ -50,8 +50,9 @@
 		allCompleted
 		inputText
 		tasks 
-		itemCompleted 
 		selectionScope 
+		//itemCompleted (not a state, but I accidentally thought it was a state in the first draft)
+
 
 	=== Step 4 Determine where each state should live
 
@@ -64,11 +65,11 @@
 	tasks affects the UI of (TaskList, Task, ControlBar)
 		-> should live in TodoPanel 
 
-	ItemCompleted affects the UI of Task)
-		-> should live in	Task
-
 	SelectionScope affects the UI of (ControlBar, TaskList)
 		-> Should live in TodoPanel
+
+	//ItemCompleted affects the UI of Task)
+	//	-> should live in	Task
 
 	Therefore, the data structure is the following 
 
@@ -81,8 +82,8 @@
 	TaskForm
 		inputText: (default "")
 
-	Task
-		itemCompleted: boolean (default: false)
+	//Task
+	//	itemCompleted: boolean (default: false)
 
 	Note that the only state provided by Meteor is tasks. Everything else is provide by React
 
